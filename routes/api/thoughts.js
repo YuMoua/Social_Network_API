f5a00685d5144605cfda2a29ce132183b2b1626f
unknown <youyu00022@gmail.com>
2024-06-02 17:41:13 -0500

    Updated Routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    updateThought
+} = require('../../controllers/thoughtsController')
 
 
+router.route('/').get(getThoughts).post(createThought);
 
-router.route('/').get()
+router.route("/:thoughtId").get(getSingleThought).delete(deleteThought).put(updateThought);
+
+// router.route('/:thoughtId/reaction').post(addReaction);
+
+// router.route('/:thoughtId/reaction/:reactionId').delete(addReaction);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+
+} = require('../../controllers/userControllers')
 
 
 
-router.route('/').get()
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
